1e32ce2f3062e6e74ecd45b790df9e3d29473f73
Jasper Bacani <jaba008@ess.ais.ac.nz>
2023-06-25 22:34:45 +1200

    This is working - moving back the declare variables in mainActivity.kt inside

--- WhatIsThisApp/app/src/main/java/com/example/whatisthisapp/MainActivity.kt
+++ WhatIsThisApp/app/src/main/java/com/example/whatisthisapp/MainActivity.kt
@@ -8,32 +8,35 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import android.os.Handler
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
+
 
     @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Splash Screen
+        Thread.sleep(3000)
+        installSplashScreen()
+
        // setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
 
-
-        //Declaring variables
-        val btnContinue = findViewById<Button>(R.id.btn_continue)
-        val etName = findViewById<EditText>(R.id.et_name)
-
-        //---------------------------------------------------
-
         //Hide Status bars and Make it Full Screen
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        //Declaring variables
+         val btnContinue: Button = findViewById<Button>(R.id.btn_continue)
+         val etName = findViewById<EditText>(R.id.et_name)
 
         btnContinue.setOnClickListener {
 
             if (etName.text.toString().isEmpty()) {
                 Toast.makeText(this,"Please enter your name", Toast.LENGTH_SHORT).show()
             } else {
-                val intent: Intent = Intent(this, QuizQuestionsActivity::class.java)
+                val intent: Intent = Intent(this, QuizSettings::class.java)
                 startActivity(intent)
                 finish()
             }
